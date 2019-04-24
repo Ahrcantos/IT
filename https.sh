fef6b31249ca63846a19378bb063fe16b92305d4
Ahrcantos <ahrcantos99@gmail.com>
2019-04-24 05:29:23 +0200

    SSL to Config

--- https.sh
+++ https.sh
@@ -6,7 +6,7 @@
 sed -i -e "s/Listen 80//g" /etc/apache2/ports.conf
 
 #Create self signed Certificate
-cd /var/www/certi
+cd /var/www/cert
 #Rsa Key
 openssl genrsa -out domain.key 2048
 #CSR
@@ -18,3 +18,13 @@ openssl x509 -req -in domain.csr -out domain.crt -days 365
 
 #Add SSL to Apache2 Config
 
+#Load Module SSL
+echo "LoadModule ssl_module modules/mod_ssl.so" >> /etc/apache2/apache2.conf
+
+#SSL on port 443
+echo "<VirtualHost *:443>" >> /etc/apache2/apache2.conf
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
+echo "SSLEngine on" >> /etc/apache2/apache2.conf
+echo "SSLCertificateFile \"/var/www/cert/domain.crt\"" >> /etc/apache2/apache2.conf
+echo "SSLCertificateKeyFile \"/var/www/cert/domain.key\"" >> /etc/apache2/apache2.conf
+echo "</VirtualHost>" >> /etc/apache2/apache2.conf
